2dd195d2db6f2f741f0c64c3768b447c7342c952
siwinlo <41877484+siwinlo@users.noreply.github.com>
2019-04-25 09:28:23 +0800

    Add files via upload

---
+++ 01_dataTypes.js
@@ -0,0 +1,15 @@
+
+    // Declare second integer, double, and String variables.
+    let secondInt, secondDouble, secondString;
+    // Read and save an integer, double, and String to your variables.
+    secondInt = parseInt(readLine());
+    secondDouble = parseFloat(readLine());
+    secondString = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(secondInt + i);
+    // Print the sum of the double variables on a new line.
+    console.log((d + secondDouble).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    console.log(s + secondString);
+    // The 's' variable above should be printed first.
+
