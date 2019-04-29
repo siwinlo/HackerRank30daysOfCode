7f953ebf39b90af4e3c2d2e357c0068f6979dce9
siwinlo <41877484+siwinlo@users.noreply.github.com>
2019-04-29 21:09:08 +0800

    Add files via upload

--- 10_binaryNumbers.js
+++ 10_binaryNumbers.js
@@ -1,19 +1,24 @@
-function main() {
+function main() { 
     const n = parseInt(readLine(), 10);
     const countArray = [];
     let count = 0;
+    // this converts the string into binary and splits it into an array of single digits
     let inputBinary = n.toString(2).split('');
 
     inputBinary.forEach((digit, index)=> {
+        //these two "if" statements count the 1s
         if (digit === '1') {
             count++;
+            // this if statement ends the count when we get to the last digit of the binary sequence
             if (index === inputBinary.length-1){
                 countArray.push(count);
             }
+            // if we come across a digit which is not '1' (i.e., '0'), set the counter to 0
         } else {
             countArray.push(count);
             count = 0;
         }
     });
+    // This line reverses countArray so we see the largest value first
     console.log(countArray.sort((a, b) => b - a)[0]);
 }
