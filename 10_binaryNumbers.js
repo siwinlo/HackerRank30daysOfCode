625c590307cefb626af71be69ea0ab5e8bdc04d6
siwinlo <41877484+siwinlo@users.noreply.github.com>
2019-04-29 21:03:47 +0800

    Add files via upload

---
+++ 10_binaryNumbers.js
@@ -0,0 +1,19 @@
+function main() {
+    const n = parseInt(readLine(), 10);
+    const countArray = [];
+    let count = 0;
+    let inputBinary = n.toString(2).split('');
+
+    inputBinary.forEach((digit, index)=> {
+        if (digit === '1') {
+            count++;
+            if (index === inputBinary.length-1){
+                countArray.push(count);
+            }
+        } else {
+            countArray.push(count);
+            count = 0;
+        }
+    });
+    console.log(countArray.sort((a, b) => b - a)[0]);
+}
